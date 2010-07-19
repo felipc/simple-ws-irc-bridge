61eeef8b3ef8d332636a6b4f8b603cae8873e29b
Felipe Gomes <felipc@gmail.com>
2010-07-19 02:45:11 -0300

    multiple views

--- client/client.js
+++ client/client.js
@@ -1,7 +1,13 @@
-function Client(doc, input, output) {
+function Client(doc, input, console) {
   this.doc = doc;
   this.input = input;
-  this.output = output;
+  
+  this.views = {
+    "@@@console": console
+  };
+  
+  this.currentView = "@@@console";
+  
 }
 
 Client.prototype = {
@@ -38,6 +44,43 @@ Client.prototype = {
     let box = this.doc.createElement("div");
     box.className = "debugBox";
     box.appendChild(this.doc.createTextNode(msg));
-    this.output.insertBefore(box, this.output.firstChild);
+    let output = this.getViewFor("@@@console");
+    output.insertBefore(box, output.firstChild);
+  },
+  
+  getViewFor: function(name) {
+    if (name in this.views)
+      return this.views[name];
+
+    let newView = this.doc.createElement("div");
+    newView.className = "output";
+    newView.style.display = "none";
+    
+    this.views[name] = newView;
+    
+    this.doc.body.appendChild(newView);
+    
+    let newButton = this.doc.createElement("button");
+    newButton.appendChild(this.doc.createTextNode(name));
+    let self = this;
+    newButton.addEventListener("click", function() {
+      self.switchView(name);
+    }, false);
+    
+    this.doc.getElementById("channelSelector").appendChild(newButton);
+    
+    return newView;
+  },
+  
+  switchView: function(name) {
+    if (this.currentView == name)
+      return;
+    
+    let view = this.getViewFor(name);
+    let curView = this.getViewFor(this.currentView);
+    curView.style.display = "none";
+    view.style.display = "";
+    
+    this.currentView = name;
   }
 }
